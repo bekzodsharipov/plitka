0e5191e8487655127d191686c8693dd44d97a886
bekzodfronten <bs3328545@gmail.com>
2025-11-18 01:13:07 +0500

    push

--- js/swiper.js
+++ js/swiper.js
@@ -27,3 +27,34 @@ var swiper = new Swiper(".mySwiper", {
     }
 });
 
+
+var swiper = new Swiper(".mySwiper2", {
+    loop: true,
+    slidesPerView: 2,
+    spaceBetween: 33,
+    autoplay: {
+        delay: 2800,    
+        disableOnInteraction: false,
+    },
+    navigation: {
+        nextEl: ".swiper-button-next2",
+        prevEl: ".swiper-button-prev2",
+    },
+    breakpoints: {
+        0: {  
+            slidesPerView: 1,
+            slidesPerGroup: 1,
+            spaceBetween: 20,
+        },
+        768: {
+            slidesPerView: 2,
+            slidesPerGroup: 2,
+        },
+        1024: {
+            slidesPerView: 2,
+            slidesPerGroup: 2,
+        }
+    }
+});
+
+
